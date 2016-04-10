f6083f194a5a1128bd3c4973f61dd9373cff0509
Jamie Wong <jamie.lf.wong@gmail.com>
2016-04-11 00:17:57 +0800

    Trying to switch to cmake for use with CLion

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,15 @@
 #include <SDL2/SDL.h>
 
 int main(int argc, char* args[]) {
-    SDL_Init( SDL_INIT_EVERYTHING );
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* screen = SDL_CreateWindow("Hello World",
+                                           SDL_WINDOWPOS_UNDEFINED,
+                                           SDL_WINDOWPOS_UNDEFINED,
+                                           640, 480,
+                                           SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    SDL_Delay(2000);
+
     SDL_Quit();
     return 0;
 }
