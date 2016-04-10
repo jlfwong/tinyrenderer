4fa01c4c7fe101d470b132e964583b87964c345b
Jamie Wong <jamie.lf.wong@gmail.com>
2016-04-11 02:09:20 +0800

    CMake now working, have a window showing up! Yesssssss

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,21 @@
-#include <SDL2/SDL.h>
+#include <SDL.h>
 
 int main(int argc, char* args[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window* screen = SDL_CreateWindow("Hello World",
-                                           SDL_WINDOWPOS_UNDEFINED,
-                                           SDL_WINDOWPOS_UNDEFINED,
-                                           640, 480,
-                                           SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-    SDL_Delay(2000);
+    SDL_Window* window = SDL_CreateWindow("Hello World",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          640, 480,
+                                          SDL_WINDOW_SHOWN);
 
-    SDL_Quit();
+    SDL_Event windowEvent;
+    while (true)
+    {
+        if (SDL_PollEvent(&windowEvent))
+        {
+            if (windowEvent.type == SDL_QUIT) break;
+        }
+    }
     return 0;
 }
