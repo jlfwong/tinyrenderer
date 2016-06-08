dfa09ed60d067013344ccac36845607ecbdded46
Jamie Wong <jamie.lf.wong@gmail.com>
2016-06-08 14:11:43 -0400

    Get rid of CRTP insanity by switching some compile time safetime to runtime assertions

--- src/vec.cc
+++ src/vec.cc
@@ -2,29 +2,30 @@
 #include "vec.h"
 
 // Efficient converters between float and int variants for convenience
-template <> template <> Vec3<int>::Vec3(const Vec3<float> &v) :
-        Vec3(int(v.x() + 0.5f), int(v.y() + 0.5f), int(v.z() + 0.5f)) {}
-template <> template <> Vec3<float>::Vec3(const Vec3<int> &v) :
-        Vec3(v.x(), v.y(), v.z()) {}
-template <> template <> Vec2<int>::Vec2(const Vec2<float> &v) :
-        Vec2(int(v.x() + 0.5f), int(v.y() + 0.5f)) {}
-template <> template <> Vec2<float>::Vec2(const Vec2<int> &v) :
-        Vec2(v.x(), v.y()) {}
+template <> template <> Vec<3, int>::Vec(const Vec<3, float> &v) :
+        Vec(int(v.x() + 0.5f), int(v.y() + 0.5f), int(v.z() + 0.5f)) {}
+template <> template <> Vec<3, float>::Vec(const Vec<3, int> &v) :
+        Vec(v.x(), v.y(), v.z()) {}
+template <> template <> Vec<2, int>::Vec(const Vec<2, float> &v) :
+        Vec(int(v.x() + 0.5f), int(v.y() + 0.5f)) {}
+template <> template <> Vec<2, float>::Vec(const Vec<2, int> &v) :
+        Vec(v.x(), v.y()) {}
 
-template <typename T> Vec3<T> Vec3<T>::cross(const Vec3<T>& b) const {
-    const Vec3<T>& a = *this;
-    return Vec3<T>(
+template <size_t DIM, typename T> Vec<3, T> Vec<DIM, T>::cross(const Vec<3, T>& b) const {
+    assert(DIM == 3);
+    const Vec<DIM, T>& a = *this;
+    return Vec<3, T>(
             a.y() * b.z() - a.z() * b.y(),
             a.z() * b.x() - a.x() * b.z(),
             a.x() * b.y() - a.y() * b.x()
     );
 };
 
-template <> const bool Vec<2, int, Vec2<int>>::operator==(const Vec2<int>& other) const {
+template <> const bool Vec<2, int>::operator==(const Vec<2, int>& other) const {
     return data_[0] == other[0] &&
            data_[1] == other[1];
 };
-template <> const bool Vec<3, int, Vec3<int>>::operator==(const Vec3<int>& other) const {
+template <> const bool Vec<3, int>::operator==(const Vec<3, int>& other) const {
     return data_[0] == other[0] &&
            data_[1] == other[1] &&
            data_[2] == other[2];
@@ -34,18 +35,18 @@ const double float_eq(float a, float b) {
     return std::abs(a - b) < 1e-6;
 }
 
-template <> const bool Vec<2, float, Vec2<float>>::operator==(const Vec2<float>& other) const {
+template <> const bool Vec<2, float>::operator==(const Vec<2, float>& other) const {
     return float_eq(data_[0], other[0]) &&
            float_eq(data_[1], other[1]);
 };
-template <> const bool Vec<3, float, Vec3<float>>::operator==(const Vec3<float>& other) const {
+template <> const bool Vec<3, float>::operator==(const Vec<3, float>& other) const {
     return float_eq(data_[0], other[0]) &&
            float_eq(data_[1], other[1]) &&
            float_eq(data_[2], other[2]);
 };
 
 // Explicit instantiations
-template class Vec2<int>;
-template class Vec2<float>;
-template class Vec3<int>;
-template class Vec3<float>;
+template class Vec<2, int>;
+template class Vec<2, float>;
+template class Vec<3, int>;
+template class Vec<3, float>;
--- src/vec.h
+++ src/vec.h
@@ -10,15 +10,7 @@
 //
 // You probably don't want to use this directly. See the type aliases
 // for Vec2f, Vec2i, Vec3f, etc. at the bottom of this file.
-
-// TODO(jlfwong); The inclusion of VecT in the template arguments here makes
-// this thing an abomination. It's necessary in order to make subtraction and
-// normalized() return Vec2<T> and Vec3<T> instead of Vec<2, T>, and Vec<3, T>.
-// We care about this, because e.g. v.cross() is only defined on Vec3, not Vec2.
-//
-// There are *probably* more sensible ways of dealing with this (even code
-// dupe is probably more reasonable than using CRTP for this.
-template <size_t DIM, typename T, typename VecT> class Vec {
+template <size_t DIM, typename T> class Vec {
 public:
     Vec() {
         for (size_t i = 0; i < DIM; i++) {
@@ -26,14 +18,29 @@ public:
         }
     }
 
+    Vec(T x, T y) {
+        assert(DIM == 2);
+        data_[0] = x;
+        data_[1] = y;
+    }
+
+    Vec(T x, T y, T z) {
+        assert(DIM == 3);
+        data_[0] = x;
+        data_[1] = y;
+        data_[2] = z;
+    }
+
+    template <typename U> Vec(const Vec<DIM, U>&);
+
     const T& operator[](const size_t i) const {
         assert(i < DIM);
         return data_[i];
     }
 
-    const bool operator==(const VecT& other) const;
+    const bool operator==(const Vec<DIM, T>& other) const;
 
-    const T dot(const VecT& other) const {
+    const T dot(const Vec<DIM, T>& other) const {
         T ret = T();
         for (size_t i = 0; i < DIM; i++) {
             ret += data_[i] * other[i];
@@ -41,15 +48,15 @@ public:
         return ret;
     }
 
-    VecT operator-(const VecT& other) const {
-        VecT ret;
+    Vec<DIM, T> operator-(const Vec<DIM, T>& other) const {
+        Vec<DIM, T> ret;
         for (size_t i = 0; i < DIM; i++) {
             ret.data_[i] = data_[i] - other[i];
         }
         return ret;
     };
 
-    VecT normalized() const {
+    Vec<DIM, T> normalized() const {
         // This method won't work very well when T is int, but hopefully
         // nobody will try to do that...
 
@@ -59,19 +66,25 @@ public:
             mag_squared += data_[i] * data_[i];
         }
         T mag = sqrt(mag_squared);
-        VecT ret;
+        Vec<DIM, T> ret;
         for (size_t i = 0; i < DIM; i++) {
             ret.data_[i] = data_[i] / mag;
         }
         return ret;
     };
 
+    T x() const { return data_[0]; }
+    T y() const { return data_[1]; }
+    T z() const { assert(DIM == 3); return data_[2]; }
+
+    Vec<3, T> cross(const Vec<3, T>&) const;
+
 protected:
     T data_[DIM];
 };
 
-template <size_t DIM, typename T, typename VecT> std::ostream& operator<<(
-        std::ostream& os, const Vec<DIM, T, VecT>& v) {
+template <size_t DIM, typename T> std::ostream& operator<<(
+        std::ostream& os, const Vec<DIM, T>& v) {
     os << "Vec<" << DIM << "," << typeid(T).name() << ">(";
     for (size_t i = 0; i < DIM; i++) {
         if (i) {
@@ -82,69 +95,12 @@ template <size_t DIM, typename T, typename VecT> std::ostream& operator<<(
     os << ")";
 };
 
-template <typename T> class Vec2 : public Vec<2, T, Vec2<T> > {
-public:
-    // For converting between Vec2i and Vec2f
-    template <typename U> Vec2(const Vec2<U>&);
-
-    Vec2(T x, T y) {
-        data_[0] = x;
-        data_[1] = y;
-    }
-
-    Vec2() : Vec2(0, 0) {}
-
-    T x() const { return data_[0]; }
-    T y() const { return data_[1]; }
-
-private:
-    using Vec<2, T, Vec2<T> >::data_;
-
-    // Holy crap I can't believe you're allowed to do this.
-    // Allows base class to access private members of derived class
-    friend Vec<2, T, Vec2<T> >;
-};
-
-template <typename T> class Vec3 : public Vec<3, T, Vec3<T> > {
-public:
-    // For converting between Vec3i and Vec3f
-    template <typename U> Vec3(const Vec3<U>&);
-
-    Vec3(T x, T y, T z) {
-        data_[0] = x;
-        data_[1] = y;
-        data_[2] = z;
-    }
-
-    Vec3() : Vec3(0, 0, 0) {}
-
-    T x() const { return data_[0]; }
-    T y() const { return data_[1]; }
-    T z() const { return data_[2]; }
-
-    Vec3<T> cross(const Vec3<T>&) const;
-
-private:
-    using Vec<3, T, Vec3<T> >::data_;
-    friend Vec<3, T, Vec3<T> >;
-};
-
-template <typename T> std::ostream& operator<<(
-        std::ostream& os, const Vec2<T>& v) {
-    os << static_cast<Vec<2, T, Vec2<T> > >(v);
-};
-
-template <typename T> std::ostream& operator<<(
-        std::ostream& os, const Vec3<T>& v) {
-    os << static_cast<Vec<3, T, Vec3<T> > >(v);
-};
-
 
 // Type aliases for vectors
 
-typedef Vec2<float> Vec2f;
-typedef Vec2<int> Vec2i;
-typedef Vec3<float> Vec3f;
-typedef Vec3<int> Vec3i;
+typedef Vec<2, float> Vec2f;
+typedef Vec<2, int> Vec2i;
+typedef Vec<3, float> Vec3f;
+typedef Vec<3, int> Vec3i;
 
 #endif //TINYRENDERER_GEOMETRY_H
